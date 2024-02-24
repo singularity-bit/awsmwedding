216589b277defdb4c6909140b474ad77a9a835f6
vasile <maxdsl98@gmail.com>
2024-02-24 22:49:26 +0200

    ts ingore

--- src/app/hooks/scrollTo.ts
+++ src/app/hooks/scrollTo.ts
@@ -1,13 +1,14 @@
 import { useGSAP } from "@gsap/react";
 import gsap from 'gsap';
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-import { RefObject } from "react";
+import { MouseEventHandler, RefObject } from "react";
 gsap.registerPlugin(ScrollToPlugin);
 
 export function useScrollToPage<T>(container: RefObject<T>, pageNr: number) {
     const { contextSafe } = useGSAP({ scope: container });
 
-    const onClickHandler = contextSafe((e: Event) => {
+    //@ts-ignore
+    const onClickHandler = contextSafe((e) => {
         if (e) {
             e.preventDefault();
             gsap.to(window, {
