fdd2179c2ed9dcb5e5bed76d20a6fc1d3bdba56a
vasile <maxdsl98@gmail.com>
2024-03-03 21:31:27 +0200

    address change

--- src/app/components/third.tsx
+++ src/app/components/third.tsx
@@ -3,6 +3,7 @@ import BackgroundImage from './backgroundImage'
 import cover from '../../../public/cover2.jpeg'
 import { Monsieur_La_Doulaise } from "next/font/google";
 
+const ADDRESS = 'https://www.google.ro/maps/dir//Cioc%C3%A2rlia,+Balkan+Hwy+36,+Chi%C8%99in%C4%83u/@47.0822047,28.8278019,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0x40c97d59eb45b0f9:0xa843cf2f07ee5066!2m2!1d28.8276126!2d47.0822894?hl=ro'
 const monsieur = Monsieur_La_Doulaise({ subsets: ["latin"], weight: ['400'], });
 export default function ThirdPage() {
     return <section className={styles.panel}>
@@ -15,8 +16,8 @@ export default function ThirdPage() {
                 <h3>
                     <a
                         target='_blank'
-                        href='https://www.google.com/maps/place/Cioc%C3%A2rlia/@47.0822047,28.8278019,15z/data=!4m9!3m8!1s0x40c97d59eb45b0f9:0xa843cf2f07ee5066!5m2!4m1!1i2!8m2!3d47.0822047!4d28.8278019!16s%2Fg%2F11gbkw4d16?entry=ttu'
-                    >Сomplex de agrement Ciocârlia</a>
+                        href={ADDRESS}
+                    >Сomplex de agrement Ciocârlia <br />Chișinău, șoseaua Balcani, 36</a>
                 </h3>
 
             </div>
